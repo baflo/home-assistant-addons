da6024c11eda8d8b5ad61f8a35854ffd98600fc3
Florian Bachmann <f.bachmann@lvm.de>
2025-05-13 13:42:51 +0200

    fix(gitea_act_runner): Fixt image

--- gitea-act-runner/rootfs/run.sh
+++ gitea-act-runner/rootfs/run.sh
@@ -2,10 +2,10 @@
 
 bashio::log.info "Starting..."
 
-export GITEA_RUNNER_NAME = $(bashio::config 'runner_name')
-export GITEA_INSTANCE_URL = $(bashio::config 'instance_url')
-export GITEA_RUNNER_REGISTRATION_TOKEN = $(bashio::config 'registration_token')
-export GITEA_RUNNER_LABELS = $(bashio::config 'labels')
+export GITEA_RUNNER_NAME=$(bashio::config 'runner_name')
+export GITEA_INSTANCE_URL=$(bashio::config 'instance_url')
+export GITEA_RUNNER_REGISTRATION_TOKEN=$(bashio::config 'registration_token')
+export GITEA_RUNNER_LABELS=$(bashio::config 'labels')
 
 
 
