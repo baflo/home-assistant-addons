7029dd96f38c642e1a33ee8d72ae138b77ced558
Florian Bachmann <834350+baflo@users.noreply.github.com>
2025-05-13 12:02:47 +0200

    feat(git_act_runner): adds logs

--- gitea-act-runner/rootfs/run.sh
+++ gitea-act-runner/rootfs/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash bashio
 
+bashio::log.info "Starting..."
+
 export GITEA_RUNNER_NAME = $(bashio::config 'runner_name')
 export GITEA_INSTANCE_URL = $(bashio::config 'instance_url')
 export GITEA_RUNNER_REGISTRATION_TOKEN = $(bashio::config 'registration_token')
 export GITEA_RUNNER_LABELS = $(bashio::config 'labels')
+
+
+
+bashio::log.info "Finished configuration. Going to start act runner..."
 
 /opt/act/run.sh "$@"
